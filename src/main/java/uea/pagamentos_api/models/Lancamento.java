e450d78b66ed3c9d9c031dc2df081e034099b915
Railene batista <rbds.ecp19@uea.edu.br>
2023-03-17 13:41:53 -0400

    primeiro comite

---
+++ src/main/java/uea/pagamentos_api/models/Lancamento.java
@@ -0,0 +1,150 @@
+package uea.pagamentos_api.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import uea.pagamentos_api.moldes.enums.TipoLancamento;
+
+@Entity
+public class Lancamento implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long codigo;
+	@NotBlank(message="Descriçao é obrigatorio")
+	private String descricao;
+	
+	@NotBlank(message="Data vencimento é obrigatorio")
+	private LocalDate dataVencimento;
+	private LocalDate dataPagamento;
+	private BigDecimal valor;
+	private String observacao;
+	
+	@Enumerated(EnumType.STRING)
+	@NotBlank(message="tipo é obrigatorio")
+	private TipoLancamento tipo;
+	
+	@ManyToOne
+	@JoinColumn(name="codigo_categoria")
+	private Categoria categoria;
+	
+	@ManyToOne
+	@JoinColumn(name="codigo_pessoa")
+	private Pessoa pessoa;
+
+	public Lancamento() {
+
+	}
+
+	public Lancamento(Long codigo, String descricao, LocalDate dataVencimento, LocalDate dataPagamento,
+			BigDecimal valor, String observacao, TipoLancamento tipo, Categoria categoria, Pessoa pessoa) {
+		super();
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.dataVencimento = dataVencimento;
+		this.dataPagamento = dataPagamento;
+		this.valor = valor;
+		this.observacao = observacao;
+		this.tipo = tipo;
+		this.categoria = categoria;
+		this.pessoa = pessoa;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(LocalDate dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public LocalDate getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(LocalDate dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	public TipoLancamento getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoLancamento tipo) {
+		this.tipo = tipo;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lancamento other = (Lancamento) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
+
+}
---
+++ src/main/java/uea/pagamentos_api/moldes/enums/TipoLancamento.java
@@ -0,0 +1,6 @@
+package uea.pagamentos_api.moldes.enums;
+
+public enum TipoLancamento {
+	RECEITA,
+	DESPESA;
+}
